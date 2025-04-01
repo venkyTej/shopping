39c0e3da70e02511eeb1948e5c4e6bf5234ee3e1
venkytej <likhilikhith169@gamil.com>
2025-04-01 20:31:26 +0530

    add products in the projects

--- shopping/mainapp/urls.py
+++ shopping/mainapp/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.home, name='homepage'),
-    path('about', views.aboutview, name='aboutpage'),
 
+from . import views
+
+urlpatterns = [
+    # path('<urlPattern>', views.<viewFunction>, name = '<path_reference_name>'),
+    # path('<urlPattern>', views.<ClassBasedView>.as_view(), name= '<path_reference_name>')
+    path('', views.home, name = 'homepage'),
+    path('about', views.aboutview, name = 'aboutpage'),
+    path('products/<int:pk>', views.productDetails.as_view(), name = 'prod_details'),
+    path('products/add', views.AddProduct.as_view(), name='add_product'),
+    path('products/edit/<int:pk>', views.EditProduct.as_view(), name = 'Edit_product'),
+    path('products/edit/<int:pk>', views.DeleteProduct.as_view(), name = 'delete_product'),
+    
 ]
--- shopping/mainapp/views.py
+++ shopping/mainapp/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.template import loader
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Product
 
@@ -13,10 +15,15 @@ from .models import Product
 def home(request):
     template = loader.get_template('home.html')
     context = {
-        'products' : Product.objects.all()
+        'products' : Product.objects.all() # HERE 'P' product is capital because product is the claas name so class name start with capithal
+        
 
     }
     return HttpResponse(template.render(context, request))
+
+class productDetails(DeleteView):
+    model = Product
+    template_name = 'product_details.html'
 
 
 
@@ -28,6 +35,27 @@ def aboutview(request):
     return HttpResponse(template.render(context, request))
 
 
+# crud operatioons-----
+
+# create  product-----
+class AddProduct(CreateView):
+    model = Product
+    template_name = 'add_product.html'
+    fields ='__all__'
+    success_url ='/'
+
 
     
-    
+    # u -- update-- product----
+class EditProduct(UpdateView):
+    model = Product
+    context_object_name = 'Product'
+    template_name = 'Edit_Product.html'   
+    fields = ['img', 'price', 'stock', 'desc'] 
+    success_url = '/'
+
+# d -- delete the product-----
+class DeleteProduct(UpdateView):
+    model = Product
+    template_name = 'delete_product.html'
+    success_url = '/'
